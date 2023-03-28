7162bf1ccc6245e803425ef29ed70ff1c65c1173
“monstereat  git config --global user.email ”532160108@qq.com <532160108@qq.com>
2023-03-28 16:05:49 +0800

    feat：prettier eslint  stylelint editorconfig 简单配置

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		"plugin:vue/vue3-essential",
+		"standard-with-typescript",
+		"standard",
+		"plugin:vue/recommended"
+	],
+	overrides: [
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module"
+	},
+	plugins: [
+		"html",
+		"vue"
+	],
+	rules: {
+		/* eslint-disable quote-props */
+		// "eol-last": ["error", "never"],
+		"handle-callback-err": ["error", "err"],
+		"indent": ["error", "tab"],
+		"lines-between-class-members": ["error", "never"],
+		"multiline-ternary": ["off"],
+		"no-async-promise-executor": ["off"],
+		"no-console": ["off"],
+		"no-extend-native": ["off"],
+		"no-extra-semi": ["error"],
+		"no-new": ["off"],
+		"no-proto": ["off"],
+		"no-return-assign": ["off"],
+		"no-sequences": ["off"],
+		"no-tabs": ["off"],
+		"no-unreachable": ["off"],
+		"no-useless-constructor": ["off"],
+		"no-var": ["error"],
+		"object-curly-spacing": ["error", "always"],
+		"operator-linebreak": ["error", "before"],
+		"quotes": ["error", "double"],
+		"semi": ["error", "always"],
+		"space-before-function-paren": ["error", "never"],
+		"switch-colon-spacing": ["error"],
+		"vue/component-definition-name-casing": ["error", "kebab-case"], // vue
+		"vue/component-tags-order": ["error", { order: ["template", "script", "style"] }], // vue
+		"vue/html-indent": ["error", "tab"], // vue
+		"vue/html-self-closing": ["off"], // vue
+		"vue/max-attributes-per-line": ["off"], // vue
+		"vue/multi-word-component-names": ["off"], // vue
+		"vue/mustache-interpolation-spacing": ["error", "never"], // vue
+		"vue/singleline-html-element-content-newline": ["off"] // vue
+		/* eslint-enable quote-props */
+	}
+};
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "app",
+	components: {
+		HelloWorld
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	margin-top: 60px;
+	text-align: center;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	color: #2c3e50;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
 }
 </style>

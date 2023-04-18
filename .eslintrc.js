e4b5ba32fe2b995878c305bb004e59d9daca85ec
“monstereat  git config --global user.email ”532160108@qq.com <532160108@qq.com>
2023-04-18 17:59:44 +0800

    fix: 规则优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,13 @@ module.exports = {
     /* eslint-disable quote-props */
     // "eol-last": ["error", "never"],
     'handle-callback-err': ['error', 'err'],
-    indent: ['error', 'tab'],
+    indent: [
+      2,
+      2,
+      {
+        SwitchCase: 1
+      }
+    ],
     'lines-between-class-members': ['error', 'never'],
     'multiline-ternary': ['off'],
     'no-async-promise-executor': ['off'],
@@ -38,17 +44,15 @@ module.exports = {
     'no-var': ['error'],
     'object-curly-spacing': ['error', 'always'],
     'operator-linebreak': ['error', 'before'],
-    quotes: ['error', 'double'],
-    semi: ['error', 'always'],
     'space-before-function-paren': ['error', 'never'],
     'switch-colon-spacing': ['error'],
     'vue/component-definition-name-casing': ['error', 'kebab-case'], // vue
     'vue/component-tags-order': ['error', { order: ['template', 'script', 'style'] }], // vue
-    'vue/html-indent': ['error', 'tab'], // vue
+    'vue/html-indent': ['error', 2], // vue
     'vue/html-self-closing': ['off'], // vue
     'vue/max-attributes-per-line': ['off'], // vue
     'vue/multi-word-component-names': ['off'], // vue
-    'vue/mustache-interpolation-spacing': ['error', 'never'], // vue
+    'vue/mustache-interpolation-spacing': ['error', 'always'], // vue
     'vue/singleline-html-element-content-newline': ['off'] // vue
     /* eslint-enable quote-props */
   }

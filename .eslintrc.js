e9859818d0084f0228782fdd178b8ebec2bb9201
“monstereat  git config --global user.email ”532160108@qq.com <532160108@qq.com>
2023-04-18 17:39:17 +0800

    fix: 优化eslint html空格

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,17 +51,7 @@ module.exports = {
     'switch-colon-spacing': ['error'],
     'vue/component-definition-name-casing': ['error', 'kebab-case'], // vue
     'vue/component-tags-order': ['error', { order: ['template', 'script', 'style'] }], // vue
-    'vue/html-indent': [
-      'error',
-      2,
-      {
-        attribute: 1,
-        baseIndent: 1,
-        closeBracket: 0,
-        alignAttributesVertically: true,
-        ignores: []
-      }
-    ], // vue
+    'vue/html-indent': ['error', 2], // vue
     'vue/html-self-closing': ['off'], // vue
     'vue/max-attributes-per-line': ['off'], // vue
     'vue/multi-word-component-names': ['off'], // vue

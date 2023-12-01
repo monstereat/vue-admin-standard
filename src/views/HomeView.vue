47b4eddc14ad66772948e5d00d4f299fa1945dd3
“monstereat  git config --global user.email ”532160108@qq.com <532160108@qq.com>
2023-12-01 17:21:33 +0800

    feat：测试-业务组件库

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>这是按钮</div>
+    <demo></demo>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+// import demo from 'bwebui/lib/demo'
+export default {
+  name: 'home-view',
+  components: {
+    HelloWorld
+    // demo
+  }
+}
+</script>

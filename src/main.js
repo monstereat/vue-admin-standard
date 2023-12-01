ad8e772f69fa3cb65ef17aab1d68915dd8adc7f1
“monstereat  git config --global user.email ”532160108@qq.com <532160108@qq.com>
2023-12-01 18:30:29 +0800

    feat(custom): 测试-业务组件

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App.vue'
 import router from './router'
 import waterMark from './directives/waterMark.js'
 
+import bui from 'bwebui'
+Vue.use(bui)
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>我是按钮</div>
+    <demo></demo>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+// import {demo} from 'bwebui/lib/demo'
 
 export default {
   name: 'home-view',
   components: {
     HelloWorld
+    // demo
   }
 }
 </script>
